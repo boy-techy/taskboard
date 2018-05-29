ab742d22870444114569cc1fd933b53a941528d1
Yatindra Kumar Khandal <yatindra.kumar@tothenew.com>
2018-05-30 03:12:21 +0530

    Initial SetUp for Server Also

--- App.js
+++ App.js
@@ -1,47 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import Login from "./src/client/components/Login";
+import SimpleDesign from './src/client/components/SampleDesign';
 
 export default class App extends React.Component {
 
-  state = {
-      name: '',
-      password: ''
-  };
-
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.loginText}>
-          <Text style={{alignSelf: 'center',
-              fontSize: 20,
-              fontWeight: 'bold',}}>LOGIN</Text>
-        </View>
-
-          <View style={styles.inputContainer}>
-              <View>
-                  <Text>UserName</Text>
-                  <TextInput onChange={(value)=> this.setState({name: value})}
-                             style={styles.textInput}
-                             underlineColorAndroid={'transparent'}
-                             clearButtonMode={'never'}
-                             value={this.state.name}/>
-              </View>
-              <View>
-                  <Text>Password</Text>
-                  <TextInput onChange={(value)=> this.setState({password: value})}
-                             style={styles.textInput}
-                             underlineColorAndroid={'transparent'}
-                             secureTextEntry={true}
-                             keyboardType={'visible-password'}
-                             value={this.state.password}/>
-                  <Text style={styles.forgotPassword}>Forgot Password ?</Text>
-              </View>
-          </View>
-
-          <View style={styles.thirdBox}>
-              <Button title={'Login'} onPress={()=> console.log("Clicked")} />
-          </View>
-      </View>
+      <ScrollView style={styles.container}>
+          <Login />
+          <SimpleDesign />
+      </ScrollView>
     );
   }
 }
@@ -49,35 +18,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#79cdd4',
-        alignItems: 'center',
-        padding: 30,
-        justifyContent: 'space-around',
     },
-    loginText: {
-        alignSelf: 'stretch',
-        padding: 20,
-        margin: 10,
-    },
-    inputContainer: {
-        alignSelf: 'stretch',
-        padding: 10,
-        justifyContent: 'space-between'
-    },
-    textInput: {
-        borderWidth: 1,
-        paddingVertical: 5,
-        paddingHorizontal: 10,
-        marginBottom: 10
-    },
-    forgotPassword: {
-        alignSelf: 'flex-end',
-        color: '#b07844',
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    thirdBox: {
-        width: 'auto',
-        backgroundColor: 'green',
-    }
 });
